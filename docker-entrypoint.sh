a7799b3cf2bd88e7d939711c2f36ef180deb3af7
Raycar5 <raycar5@users.noreply.github.com>
2021-09-13 20:07:27 +0200

    add production features to entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,18 @@
-#!/bin/bash
+#!/bin/bash -xe
 
-set -Eeuo pipefail
+set -xe -o pipefail
+_term() {
+	echo "Caught SIGTERM signal!"
+	kill -TERM "$child" 2>/dev/null
+}
+trap _term SIGTERM
 
 envsubst <project.template.yaml >project.yaml
 
-(
-	npm run sql || kill "$$"
-) &
-subql-node --network-endpoint="$NETWORK_ENDPOINT" $@
+(npm run sql || (sleep 3 && kill "$$")) &
+
+node --max-old-space-size=1536 \
+	/usr/local/bin/subql-node \
+	--network-endpoint="$NETWORK_ENDPOINT" $@
+child=$!
+wait "$child"
