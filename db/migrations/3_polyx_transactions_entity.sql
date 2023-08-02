ce02d07ac6dd8be4c18cb881f2dc7ecb451a066b
Prashant Bajpai <prashant.bajpai19@gmail.com>
2023-08-02 22:56:14 +0530

    fix: 🐛 Update queries to correctly create "migrations"

--- db/migrations/3_polyx_transactions_entity.sql
+++ db/migrations/3_polyx_transactions_entity.sql
@@ -2,6 +2,10 @@ alter table migrations add column if not exists executed boolean;
 update migrations set executed = false where executed is null;
 alter table migrations alter column executed set not null;
 
+alter table migrations add column if not exists processed_block integer;
+update migrations set processed_block = 0 where processed_block is null;
+alter table migrations alter column processed_block set not null;
+
 DO $$
 BEGIN
     IF NOT EXISTS (select 1 from pg_type where typname = 'public_enum_5df0f1d22c') then
