81e2df1cfbd4324e20fcc831c4f6e2416562e612
Prashant Bajpai <prashant.bajpai19@gmail.com>
2023-11-23 23:44:33 +0530

    refactor: 💡 remove migration handler code

--- db/migrations/3_update_multisig_proposal_status.sql
+++ db/migrations/3_update_multisig_proposal_status.sql
@@ -1,2 +1,2 @@
 alter table multi_sig_proposals alter column status type text;
-create index if not exists "0x9b5ac03e59df0332" on multi_sig_proposals on btree(status);
+create index if not exists "0x9b5ac03e59df0332" on multi_sig_proposals(status);
