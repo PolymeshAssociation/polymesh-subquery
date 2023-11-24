e401ceb0e5f3153df97b9e8ecc7a415555b46964
Prashant Bajpai <prashant.bajpai19@gmail.com>
2023-11-24 18:24:33 +0530

    feat: 🎸 Add new events for 6.1.x chain

--- db/migrations/1_consts_for_6_1_0_chain.sql
+++ db/migrations/1_consts_for_6_1_0_chain.sql
@@ -3,6 +3,7 @@ alter type "0bf3c7d4ef" add value if not exists 'revoke_create_portfolios_permis
 alter type "0bf3c7d4ef" add value if not exists 'create_custody_portfolio' after 'revoke_create_portfolios_permission';
 alter type "0bf3c7d4ef" add value if not exists 'instantiate_with_code_as_primary_key' after 'create_custody_portfolio';
 alter type "0bf3c7d4ef" add value if not exists 'instantiate_with_hash_as_primary_key' after 'instantiate_with_code_as_primary_key';
+alter type "0bf3c7d4ef" add value if not exists 'upgrade_api' after 'instantiate_with_code_as_primary_key';
 
 alter type "8f5a39c8ee" add value if not exists 'RemoveAssetAffirmationExemption' after 'VenueSignersUpdated';
 alter type "8f5a39c8ee" add value if not exists 'AssetAffirmationExemption' after 'RemoveAssetAffirmationExemption';
@@ -11,3 +12,5 @@ alter type "8f5a39c8ee" add value if not exists 'RemovePreApprovedAsset' after '
 alter type "8f5a39c8ee" add value if not exists 'PreApprovedPortfolio' after 'RemovePreApprovedAsset';
 alter type "8f5a39c8ee" add value if not exists 'RevokePreApprovedPortfolio' after 'PreApprovedPortfolio';
 alter type "8f5a39c8ee" add value if not exists 'InstructionAutomaticallyAffirmed' after 'RevokePreApprovedPortfolio';
+alter type "8f5a39c8ee" add value if not exists 'ApiHashUpdated' after 'DelegateCalled';
+alter type "8f5a39c8ee" add value if not exists 'SCRuntimeCall' after 'ApiHashUpdated';
