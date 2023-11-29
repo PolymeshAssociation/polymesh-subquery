7cf28bf6d16ae85e805909987d85650fe444239e
Prashant Bajpai <prashant.bajpai19@gmail.com>
2023-11-29 21:04:02 +0530

    fix: 🐛 Correct the sorting of the CallIdEnum
    This requires removing all the migrations, which makes this the base
    version moving forward for chains 6.1 and higher
    
    BREAKING CHANGE: 🧨 Removes old migrations and forces to resync SQ

--- db/migrations/4_reset_proposal_status_to_active.sql
+++
@@ -1,7 +0,0 @@
--- upgrade_api was incorrectly sorted if SQ upgrades were used after 10.2.0
--- Below queries will delete the manual insertion and fix the API
-delete from pg_enum where enumlabel = 'upgrade_api'; 
-alter type "0bf3c7d4ef" add value if not exists 'upgrade_api' after 'instantiate_with_hash_as_primary_key';
-
--- status of proposals was incorrectly set to `Deleted` on SQ restarts. We set all the deleted proposals to `Active` and then updated logic will handle them correctly.
-update multi_sig_proposals set status = 'Active' where status = 'Deleted';
