a2e1b4bbc528184b1345156609305db154bc57b2
Toms Veidemanis <toms@veidemanis.lv>
2024-08-26 15:55:27 +0300

    fix: 🐛 frozenForAsset not itrable

--- src/mappings/entities/mapConfidentialAccountCreated.ts
+++ src/mappings/entities/mapConfidentialAccountCreated.ts
@@ -25,37 +25,69 @@ export const handleConfidentialAccountCreated = async (event: SubstrateEvent): P
 export const handleConfidentialAssetFrozenForAccount = async (
   event: SubstrateEvent
 ): Promise<void> => {
-  const { blockId, params } = extractArgs(event);
-  const [, rawAccount, rawAsset] = params;
+  const { blockId, params, eventIdx } = extractArgs(event);
+  const [rawCreator, rawAccount, rawAsset] = params;
 
   const accountId = getTextValue(rawAccount);
   const assetId = getTextValue(rawAsset);
 
   const account = await ConfidentialAccount.get(accountId);
 
-  if (account && !account.frozenForAsset.includes(assetId)) {
+  if (account && !Array.isArray(account.frozenForAsset)) {
+    account.frozenForAsset = [assetId];
+
+    await account.save();
+  } else if (account && !account.frozenForAsset.includes(assetId)) {
     account.frozenForAsset = [...account.frozenForAsset, assetId];
     account.updatedBlockId = blockId;
 
     await account.save();
+  } else if (!account) {
+    const creatorId = getTextValue(rawCreator);
+
+    await ConfidentialAccount.create({
+      id: accountId,
+      account: accountId,
+      creatorId,
+      eventIdx,
+      frozenForAsset: [assetId],
+      createdBlockId: blockId,
+      updatedBlockId: blockId,
+    }).save();
   }
 };
 
 export const handleConfidentialAssetUnfrozenForAccount = async (
   args: HandlerArgs
 ): Promise<void> => {
-  const { blockId, params } = args;
-  const [, rawAccount, rawAsset] = params;
+  const { blockId, params, eventIdx } = args;
+  const [rawCreator, rawAccount, rawAsset] = params;
 
   const accountId = getTextValue(rawAccount);
   const assetId = getTextValue(rawAsset);
 
   const account = await ConfidentialAccount.get(accountId);
 
-  if (account?.frozenForAsset.includes(assetId)) {
+  if (account && !Array.isArray(account.frozenForAsset)) {
+    account.frozenForAsset = [];
+
+    await account.save();
+  } else if (account?.frozenForAsset.includes(assetId)) {
     account.frozenForAsset = account.frozenForAsset.filter(id => id !== assetId);
     account.updatedBlockId = blockId;
 
     await account.save();
+  } else if (!account) {
+    const creatorId = getTextValue(rawCreator);
+
+    await ConfidentialAccount.create({
+      id: accountId,
+      account: accountId,
+      creatorId,
+      eventIdx,
+      frozenForAsset: [],
+      createdBlockId: blockId,
+      updatedBlockId: blockId,
+    }).save();
   }
 };
