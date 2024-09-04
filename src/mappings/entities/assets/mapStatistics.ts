caa2ac8e22baa0ab46c1d259453327d3de56659a
Prashant Bajpai <34747455+prashantasdeveloper@users.noreply.github.com>
2024-09-04 13:55:02 +0530

    fix: 🐛 Handle transfer compliance for zero as stat value

--- src/mappings/entities/assets/mapStatistics.ts
+++ src/mappings/entities/assets/mapStatistics.ts
@@ -110,7 +110,7 @@ const getTransferConditions = (item: Codec, assetId: string): Attributes<Transfe
   const transferConditions = JSON.parse(item.toString()) as MeshTransferCondition[];
   return transferConditions.map(
     ({ maxInvestorCount, maxInvestorOwnership, claimOwnership, claimCount }) => {
-      if (maxInvestorCount) {
+      if (maxInvestorCount !== undefined) {
         return {
           assetId,
           type: TransferComplianceTypeEnum.MaxInvestorCount,
@@ -118,7 +118,7 @@ const getTransferConditions = (item: Codec, assetId: string): Attributes<Transfe
           value: BigInt(maxInvestorCount),
         };
       }
-      if (maxInvestorOwnership) {
+      if (maxInvestorOwnership !== undefined) {
         return {
           assetId,
           type: TransferComplianceTypeEnum.MaxInvestorOwnership,
@@ -126,7 +126,7 @@ const getTransferConditions = (item: Codec, assetId: string): Attributes<Transfe
           value: BigInt(maxInvestorOwnership),
         };
       }
-      if (claimCount) {
+      if (claimCount !== undefined) {
         const { type: claimType, value: claimValue } = getStatClaim(claimCount[0]);
         const claimIssuerId = claimCount[1];
         return {
@@ -140,7 +140,7 @@ const getTransferConditions = (item: Codec, assetId: string): Attributes<Transfe
           max: BigInt(claimCount[3]),
         };
       }
-      if (claimOwnership) {
+      if (claimOwnership !== undefined) {
         const { type: claimType, value: claimValue } = getStatClaim(claimOwnership[0]);
         const claimIssuerId = claimOwnership[1];
         return {
